62b66c34cc23c0fe5c00730968c44404f461264d
LukasRimk <lukas.rmk@gmail.com>
2019-03-03 20:46:24 +0200

    Added level transition

---
+++ Assets/Scripts/levelChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelChanger : MonoBehaviour
+{
+    public int level;
+
+    private void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+
+}
