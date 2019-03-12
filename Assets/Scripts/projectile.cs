218bdb4d7d617658304548518a10d911c5fc5f64
LukasRimk <lukas.rmk@gmail.com>
2019-03-12 13:19:11 +0200

    GAM2

---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class projectile : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float random = Random.Range(-1, 1);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(player.position.x + random, player.position.y + random);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjectile();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            DestroyProjectile();
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+
+        if (collision.CompareTag("wall"))
+        {
+            DestroyProjectile();
+        }
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
